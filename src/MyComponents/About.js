c058c3b04419bd992c83456afb66edb089480c33
sushil <ghimires2054@gmail.com>
2021-05-10 15:49:52 +0545

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>Hello guy's please contact us for free learner about programming language</p>
+        </div>
+    )
+}
